46f4f58fe47efdf5002a4f15468cf597cb1d4236
Norbert Schramm <stoertebeker@kkb-clan.de>
2020-05-24 15:17:02 +0200

    updated vars

--- generator.sh
+++ generator.sh
@@ -5,7 +5,9 @@ x=2
 
 ### edit this vars
 num_clients=10
-vpn_server=vpn.example.com:51820
+vpn_server=vpn.example.com
+vpn_port=51820
+server_local_if=eth0
 ### edit end
 
 num_clients=$(( $num_clients + 1 ))
@@ -18,7 +20,9 @@ sPRIV=$(wg genkey) && sPUB=$(echo $sPRIV | wg pubkey)
 cat << EOF > server.conf
 [Interface]
 Address = 10.7.38.1/24
-ListenPort = 51820
+PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -t nat -A POSTROUTING -o $server_local_if -j MASQUERADE
+PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -t nat -D POSTROUTING -o $server_local_if -j MASQUERADE
+ListenPort = $vpn_port
 PrivateKey = $sPRIV
 
 EOF
@@ -37,7 +41,7 @@ PrivateKey = $PRIV
 
 [Peer]
 PublicKey = $sPUB
-Endpoint = $vpn_server
+Endpoint = $vpn_server:$vpn_port
 AllowedIPs = 10.7.38.0/24
 
 PersistentKeepalive = 25
